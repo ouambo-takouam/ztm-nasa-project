12a9f1d2e757c47ccd1e17fdf29a718f59c44295
patrick ouambo <patrickouambo@gmail.com>
2023-04-08 17:19:35 +0200

    starting configuring

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,8 +7,22 @@ const { loadPlanetsData } = require('./models/planets/planets.model');
 
 const PORT = 8000;
 
+const MONGO_URL =
+	'mongodb+srv://nasa-api:QFX83cwEz8JMoGJF@nasacluster.u4hkq82.mongodb.net/nasa?retryWrites=true&w=majority';
+
+// event listeners
+mongoose.connection.once('open', () => {
+	console.log('Connection to mongo atlas open');
+});
+
+mongoose.connection.once('error', (error) => {
+	console.error(error);
+});
+
 const server = http.createServer(app);
 
+// Establish connection to mongodb before running server
+mongoose.connect(MONGO_URL);
 loadPlanetsData();
 
 server.listen(PORT, () => {
