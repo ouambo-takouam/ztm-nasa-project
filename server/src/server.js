1563df5b5a03e9af495bfff623268179e7f9f172
patrick ouambo <patrickouambo@gmail.com>
2023-04-10 12:28:42 +0200

    environment variables set

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const http = require('http');
 
 const app = require('./app');
@@ -7,7 +9,7 @@ const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets/planets.model');
 const { loadLaunchesData } = require('./models/launches/launches.model');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
